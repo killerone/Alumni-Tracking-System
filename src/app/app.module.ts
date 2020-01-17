cc0febe9cba50dac78a7f10fc485469a47433b10
Anuradha <anudate97@gmail.com>
2020-01-17 09:43:46 +0530

    notice-board

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,10 +10,11 @@ import { UserNavBarComponent } from './user-nav-bar/user-nav-bar.component';
 import { ClgNavBarComponent } from './clg-nav-bar/clg-nav-bar.component';
 import { UniNavBarComponent } from './uni-nav-bar/uni-nav-bar.component';
 import { EventComponent } from './event/event.component';
+import { NoticeBoardComponent } from './notice-board/notice-board.component';
 
 
 @NgModule({
-  declarations: [AppComponent, TopBarComponent, UserNavBarComponent, ClgNavBarComponent, UniNavBarComponent, EventComponent],
+  declarations: [AppComponent, TopBarComponent, UserNavBarComponent, ClgNavBarComponent, UniNavBarComponent, EventComponent, NoticeBoardComponent],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, HttpClientModule],
   providers: [
     // { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
